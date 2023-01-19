c8e1cb3b9f888c1751746cdf4c8c022a0f906114
Diego <diegoariel79@hotmail.com>
2023-01-19 06:42:52 -0300

    Archivo Dockerfile

--- src/main/java/com/portafolio/diego2/Repository/Rhys.java
+++ src/main/java/com/portafolio/diego2/Repository/Rhys.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -19,4 +20,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface Rhys extends JpaRepository<hys, Integer>{
     Optional<hys> findByNombre(String nombre);
     public boolean existsByNombre(String nombre);
+=======
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portafolio.diego2.Repository;
+
+/**
+ *
+ * @author Diego Roldan
+ */
+import com.portafolio.diego2.Entity.hys;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface Rhys extends JpaRepository<hys, Integer>{
+    Optional<hys> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+>>>>>>> 2fab75b (Se agregaron modulos porcentaje y educación)
 }
--- src/main/java/com/portafolio/diego2/Security/Controller/AuthController.java
+++ src/main/java/com/portafolio/diego2/Security/Controller/AuthController.java
@@ -27,7 +27,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-//@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
